24e56260894895636cc6822b9adff6b98526e0e7
deyan.tsvetkov <deyan.tsvetkow@dreamix.eu>
2026-07-05 09:19:54 +0300

    Add path alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -7,5 +8,10 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
     global: "window",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
